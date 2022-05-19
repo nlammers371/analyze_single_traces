3b2f6ecceffd7e8e609c8c29087758356f48147e
nlammers371 <nlammers@berkeley.edu>
2022-05-18 21:43:04 -0700

    First commit. Adding core scripts from tf_enrichment_pipeline codebase

---
+++ src/cpHMM_scripts/call_inference_savio.m
@@ -0,0 +1,9 @@
+% wrapper function to call the inference
+clear 
+close all
+
+% move to inference directory
+cd('~/repos/analyze_single_traces/cpHMM_scripts');
+
+% call the inference function
+main05_conduct_cpHMM_inference;
